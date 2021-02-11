db0bf8e23542d2e460eeb14dd35751995137a592
Rye Gilliss <gilliss.rg@gmail.com>
2021-02-11 17:39:55 +0000

    refactored

--- fetcher.js
+++ fetcher.js
@@ -4,26 +4,20 @@ const request = require('request');
 
 
 const url = arg[0];
-const file = arg[1];
-let write = "";
-// let downloaded = fs.statSync(file);
-// const size = downloaded.size
-request(url, (error, response, body) => {
+const location = arg[1];
+
+request(url, function(error, response, body) {
+  let copy = "";
   console.log('error:', error); 
   console.log('statusCode:', response && response.statusCode);
-  write += body;
-  return write;
-});
-
-
-
-setTimeout(() => {
-  fs.writeFile(file, write, function(err) {
+  copy += body;
+  fs.writeFile(location, copy, function(err) {
     if(err) {
       return console.log(err);
     }
-    console.log(`Downloaded and saved to ${file}`)
+    console.log(`Downloaded and saved to ${location}`)
   });
+});
 
-}, 500);
+
 
